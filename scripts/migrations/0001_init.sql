35ab224411fd0c2a11ebfef41aede8f6884f7ae7
Raykhanov Aksultan Serikovich <araykhanov@ozon.ru>
2023-04-03 17:12:10 +0300

    added constraint

--- scripts/migrations/0001_init.sql
+++ scripts/migrations/0001_init.sql
@@ -2,16 +2,17 @@
 -- +goose StatementBegin
 CREATE TABLE kitchen_order
 (
-    id                  BIGSERIAL PRIMARY KEY NOT NULL,
-    shop_order_id       BIGINT                NOT NULL,
-    status              TEXT                  NOT NULL
+    id            BIGSERIAL PRIMARY KEY NOT NULL,
+    shop_order_id BIGINT UNIQUE         NOT NULL,
+    status        TEXT                  NOT NULL,
+    CONSTRAINT shop_order_id UNIQUE (shop_order_id)
 );
 
 CREATE TABLE item
 (
-    kitchen_order_id    BIGINT REFERENCES kitchen_order(id),
-    item_name           TEXT                  NOT NULL,
-    item_comment        TEXT                  NULL
+    kitchen_order_id BIGINT REFERENCES kitchen_order (id),
+    item_name        TEXT NOT NULL,
+    item_comment     TEXT NULL
 );
 -- +goose StatementEnd
 
